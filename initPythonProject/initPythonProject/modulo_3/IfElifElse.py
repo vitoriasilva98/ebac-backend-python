85d26bcd2f70180b9c91e6ce5ea438b36c9c78af
Vitória Vitoria <vitoriasilva98@hotmail.com>
2024-09-14 14:24:29 -0300

    Módulo 3 - Fluxo Condicional & Repetição (finalizado)

---
+++ initPythonProject/initPythonProject/modulo_3/IfElifElse.py
@@ -0,0 +1,10 @@
+codigo_de_seguranca = '852'
+codigo_de_seguranca_cadastro = '852'
+senha = '7783'
+senha_cadastro = '7783'
+
+if (codigo_de_seguranca == codigo_de_seguranca_cadastro) & \
+        (senha == senha_cadastro):
+    print("Pagamento efetuado")
+else:
+    print("Erro: Pagamento não efetuado")
